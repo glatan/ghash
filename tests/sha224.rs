2cab716cf279f5d1b491bb91a6cf01ea33ecfbc3
glatan <glatan.edu@gmail.com>
2020-04-17 02:01:12 +0900

    split sha2 tests

---
+++ tests/sha224.rs
@@ -0,0 +1,27 @@
+extern crate ghash;
+
+use ghash::*;
+
+// https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/SHA224.pdf
+// SHA224 ("abc") = 23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7
+// SHA224 ("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq") = 75388b16512776cc5dba5da1fd890150b0c6455cb4f58b1952522525
+#[test]
+fn sha224_1() {
+    let expected = "23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7";
+    let sha224 = Sha224::hash("abc".as_bytes())
+        .iter()
+        .map(|byte| format!("{:02x}", byte))
+        .collect::<String>();
+    assert_eq!(sha224, expected);
+}
+
+#[test]
+fn sha224_2() {
+    let expected = "75388b16512776cc5dba5da1fd890150b0c6455cb4f58b1952522525";
+    let sha224 =
+        Sha224::hash("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".as_bytes())
+            .iter()
+            .map(|byte| format!("{:02x}", byte))
+            .collect::<String>();
+    assert_eq!(sha224, expected);
+}
---
+++ tests/sha512.rs
@@ -0,0 +1,23 @@
+extern crate ghash;
+
+use ghash::*;
+
+// https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/SHA512.pdf
+// SHA512("abc") = ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f
+// SHA512("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu") = 8e959b75dae313da8cf4f72814fc143f8f7779c6eb9f7fa17299aeadb6889018501d289e4900f7e4331b99dec4b5433ac7d329eeb6dd26545e96e55b874be909
+#[test]
+fn sha512_1() {
+    let expected = "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f";
+    let sha512 = Sha512::hash("abc".as_bytes())
+        .iter()
+        .map(|byte| format!("{:02x}", byte))
+        .collect::<String>();
+    assert_eq!(sha512, expected);
+}
+
+#[test]
+fn sha512_2() {
+    let expected = "8e959b75dae313da8cf4f72814fc143f8f7779c6eb9f7fa17299aeadb6889018501d289e4900f7e4331b99dec4b5433ac7d329eeb6dd26545e96e55b874be909";
+    let sha512 = Sha512::hash("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu".as_bytes()).iter().map(|byte| format!("{:02x}", byte)).collect::<String>();
+    assert_eq!(sha512, expected);
+}
