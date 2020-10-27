5fbc6ef75c047400c6c5649bd550d206cd5e9989
glatan <glatan@protonmail.com>
2020-10-27 19:13:17 +0900

    minimize macro call

--- components/hashes/ripemd/src/default/ripemd128.rs
+++ components/hashes/ripemd/src/default/ripemd128.rs
@@ -5,29 +5,33 @@ use utils::{impl_md_flow, uint_from_bytes, Hash};
 use std::cmp::Ordering;
 
 macro_rules! round_left {
-    ($j:expr, $t:expr, $a:expr, $b:expr, $c:expr, $d:expr, $f:ident, $x:expr) => {
-        $t = $a
-            .wrapping_add($f($b, $c, $d))
-            .wrapping_add($x[R_LEFT[$j]])
-            .wrapping_add(K128_LEFT[($j / 16)])
-            .rotate_left(S_LEFT[$j]);
-        $a = $d;
-        $d = $c;
-        $c = $b;
-        $b = $t;
+    ($t:expr, $a:expr, $b:expr, $c:expr, $d:expr, $f:ident, $x:expr, $( $j:expr ),+) => {
+        $(
+            $t = $a
+                .wrapping_add($f($b, $c, $d))
+                .wrapping_add($x[R_LEFT[$j]])
+                .wrapping_add(K128_LEFT[($j / 16)])
+                .rotate_left(S_LEFT[$j]);
+            $a = $d;
+            $d = $c;
+            $c = $b;
+            $b = $t;
+        )*
     };
 }
 macro_rules! round_right {
-    ($j:expr, $t:expr, $a:expr, $b:expr, $c:expr, $d:expr, $f:ident, $x:expr) => {
-        $t = $a
-            .wrapping_add($f($b, $c, $d))
-            .wrapping_add($x[R_RIGHT[$j]])
-            .wrapping_add(K128_RIGHT[($j / 16)])
-            .rotate_left(S_RIGHT[$j]);
-        $a = $d;
-        $d = $c;
-        $c = $b;
-        $b = $t;
+    ($t:expr, $a:expr, $b:expr, $c:expr, $d:expr, $f:ident, $x:expr, $( $j:expr ),+) => {
+        $(
+            $t = $a
+                .wrapping_add($f($b, $c, $d))
+                .wrapping_add($x[R_RIGHT[$j]])
+                .wrapping_add(K128_RIGHT[($j / 16)])
+                .rotate_left(S_RIGHT[$j]);
+            $a = $d;
+            $d = $c;
+            $c = $b;
+            $b = $t;
+        )*
     };
 }
 
@@ -44,137 +48,41 @@ impl Ripemd128 {
         let [mut a_left, mut b_left, mut c_left, mut d_left] = self.status;
         let [mut a_right, mut b_right, mut c_right, mut d_right] = self.status;
         // Round 1
-        round_left!(0, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(1, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(2, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(3, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(4, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(5, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(6, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(7, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(8, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(9, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(10, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(11, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(12, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(13, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(14, t, a_left, b_left, c_left, d_left, f1, x);
-        round_left!(15, t, a_left, b_left, c_left, d_left, f1, x);
-        round_right!(0, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(1, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(2, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(3, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(4, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(5, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(6, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(7, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(8, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(9, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(10, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(11, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(12, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(13, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(14, t, a_right, b_right, c_right, d_right, f4, x);
-        round_right!(15, t, a_right, b_right, c_right, d_right, f4, x);
+        round_left!(
+            t, a_left, b_left, c_left, d_left, f1, x, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+            14, 15
+        );
+        round_right!(
+            t, a_right, b_right, c_right, d_right, f4, x, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+            13, 14, 15
+        );
         // Round 2
-        round_left!(16, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(17, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(18, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(19, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(20, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(21, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(22, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(23, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(24, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(25, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(26, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(27, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(28, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(29, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(30, t, a_left, b_left, c_left, d_left, f2, x);
-        round_left!(31, t, a_left, b_left, c_left, d_left, f2, x);
-        round_right!(16, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(17, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(18, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(19, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(20, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(21, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(22, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(23, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(24, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(25, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(26, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(27, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(28, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(29, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(30, t, a_right, b_right, c_right, d_right, f3, x);
-        round_right!(31, t, a_right, b_right, c_right, d_right, f3, x);
+        round_left!(
+            t, a_left, b_left, c_left, d_left, f2, x, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+            27, 28, 29, 30, 31
+        );
+        round_right!(
+            t, a_right, b_right, c_right, d_right, f3, x, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+            26, 27, 28, 29, 30, 31
+        );
         // Round 3
-        round_left!(32, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(33, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(34, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(35, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(36, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(37, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(38, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(39, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(40, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(41, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(42, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(43, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(44, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(45, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(46, t, a_left, b_left, c_left, d_left, f3, x);
-        round_left!(47, t, a_left, b_left, c_left, d_left, f3, x);
-        round_right!(32, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(33, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(34, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(35, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(36, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(37, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(38, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(39, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(40, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(41, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(42, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(43, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(44, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(45, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(46, t, a_right, b_right, c_right, d_right, f2, x);
-        round_right!(47, t, a_right, b_right, c_right, d_right, f2, x);
+        round_left!(
+            t, a_left, b_left, c_left, d_left, f3, x, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+            43, 44, 45, 46, 47
+        );
+        round_right!(
+            t, a_right, b_right, c_right, d_right, f2, x, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+            42, 43, 44, 45, 46, 47
+        );
         // Round 4
-        round_left!(48, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(49, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(50, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(51, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(52, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(53, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(54, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(55, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(56, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(57, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(58, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(59, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(60, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(61, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(62, t, a_left, b_left, c_left, d_left, f4, x);
-        round_left!(63, t, a_left, b_left, c_left, d_left, f4, x);
-        round_right!(48, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(49, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(50, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(51, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(52, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(53, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(54, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(55, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(56, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(57, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(58, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(59, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(60, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(61, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(62, t, a_right, b_right, c_right, d_right, f1, x);
-        round_right!(63, t, a_right, b_right, c_right, d_right, f1, x);
+        round_left!(
+            t, a_left, b_left, c_left, d_left, f4, x, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58,
+            59, 60, 61, 62, 63
+        );
+        round_right!(
+            t, a_right, b_right, c_right, d_right, f1, x, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+            58, 59, 60, 61, 62, 63
+        );
 
         t = self.status[1].wrapping_add(c_left).wrapping_add(d_right);
         self.status[1] = self.status[2].wrapping_add(d_left).wrapping_add(a_right);
