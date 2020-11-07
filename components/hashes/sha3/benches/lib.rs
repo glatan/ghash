fab0c3f7f4683eaa3c3d1c8a4f7be5a7f6b9a650
glatan <glatan@protonmail.com>
2020-11-07 23:48:35 +0900

    impl Benchmark for SHAKE

--- components/hashes/sha3/benches/lib.rs
+++ components/hashes/sha3/benches/lib.rs
@@ -6,3 +6,5 @@ impl_benchmark!(sha3, Sha3_224);
 impl_benchmark!(sha3, Sha3_256);
 impl_benchmark!(sha3, Sha3_384);
 impl_benchmark!(sha3, Sha3_512);
+impl_benchmark!(sha3, Shake128);
+impl_benchmark!(sha3, Shake256);
