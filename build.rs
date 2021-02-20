45fd0c3a0b7f2032f492579945d0d96e34b0c366
glatan <glatan@protonmail.com>
2021-02-21 01:13:19 +0900

    refactoring

--- build.rs
+++ build.rs
@@ -17,7 +17,13 @@ fn main() {
     // Target : aarch64-unknown-linux-musl
     if rustc_version.0 == 1
         && rustc_version.1 < 48
-        && is_aarch64_unknown_linux_musl(&target_arch, &target_vendor, &target_os, &target_env, &target_endian)
+        && is_aarch64_unknown_linux_musl(
+            &target_arch,
+            &target_vendor,
+            &target_os,
+            &target_env,
+            &target_endian,
+        )
     {
         let gcc_libdir = get_gcc_library_path("aarch64-linux-gnu-gcc");
         println!("cargo:rustc-link-search=native={}", gcc_libdir);
@@ -27,7 +33,13 @@ fn main() {
     // Target : mips-unknown-linux-musl
     if rustc_version.0 == 1
         && rustc_version.1 < 47
-        && is_mips_unknown_linux_musl(&target_arch, &target_vendor, &target_os, &target_env, &target_endian)
+        && is_mips_unknown_linux_musl(
+            &target_arch,
+            &target_vendor,
+            &target_os,
+            &target_env,
+            &target_endian,
+        )
     {
         let gcc_libdir = get_gcc_library_path("mips-linux-gnu-gcc");
         println!("cargo:rustc-link-search=native={}", gcc_libdir);
@@ -37,7 +49,13 @@ fn main() {
     // Target : mips64-unknown-linux-muslabi64
     if rustc_version.0 == 1
         && rustc_version.1 < 48
-        && is_mips64_unknown_linux_muslabi64(&target_arch, &target_vendor, &target_os, &target_env, &target_endian)
+        && is_mips64_unknown_linux_muslabi64(
+            &target_arch,
+            &target_vendor,
+            &target_os,
+            &target_env,
+            &target_endian,
+        )
     {
         let gcc_libdir = get_gcc_library_path("mips64-linux-gnuabi64-gcc");
         println!("cargo:rustc-link-search=native={}", gcc_libdir);
@@ -47,7 +65,13 @@ fn main() {
     // Target : mips64el-unknown-linux-muslabi64
     if rustc_version.0 == 1
         && rustc_version.1 < 48
-        && is_mips64el_unknown_linux_muslabi64(&target_arch, &target_vendor, &target_os, &target_env, &target_endian)
+        && is_mips64el_unknown_linux_muslabi64(
+            &target_arch,
+            &target_vendor,
+            &target_os,
+            &target_env,
+            &target_endian,
+        )
     {
         let gcc_libdir = get_gcc_library_path("mips64el-linux-gnuabi64-gcc");
         println!("cargo:rustc-link-search=native={}", gcc_libdir);
@@ -65,15 +89,10 @@ fn get_rustc_version() -> (u32, u32, u32) {
         .unwrap()
         .split(" ")
         .collect::<Vec<&str>>()[1];
-    let major_version = u32::from_str(version_str.split(".").collect::<Vec<&str>>()[0])
-        .ok()
-        .unwrap();
-    let minor_version = u32::from_str(version_str.split(".").collect::<Vec<&str>>()[1])
-        .ok()
-        .unwrap();
-    let patch_version = u32::from_str(version_str.split(".").collect::<Vec<&str>>()[2])
-        .ok()
-        .unwrap();
+    let splitted_version = version_str.split(".").collect::<Vec<&str>>();
+    let major_version = u32::from_str(splitted_version[0]).ok().unwrap();
+    let minor_version = u32::from_str(splitted_version[1]).ok().unwrap();
+    let patch_version = u32::from_str(splitted_version[2]).ok().unwrap();
     (major_version, minor_version, patch_version)
 }
 
@@ -95,29 +114,73 @@ fn get_gcc_library_path(cmd: &str) -> String {
     path.to_owned()
 }
 
-fn is_aarch64_unknown_linux_musl(target_arch: &str, target_vendor: &str, target_os: &str, target_env: &str, target_endian: &str) -> bool {
-    if target_arch == "aarch64" && target_vendor == "unknown" && target_os == "linux" && target_env == "musl" && target_endian == "little" {
+fn is_aarch64_unknown_linux_musl(
+    target_arch: &str,
+    target_vendor: &str,
+    target_os: &str,
+    target_env: &str,
+    target_endian: &str,
+) -> bool {
+    if target_arch == "aarch64"
+        && target_vendor == "unknown"
+        && target_os == "linux"
+        && target_env == "musl"
+        && target_endian == "little"
+    {
         return true;
     }
     false
 }
 
-fn is_mips_unknown_linux_musl(target_arch: &str, target_vendor: &str, target_os: &str, target_env: &str, target_endian: &str) -> bool {
-    if target_arch == "mips" && target_vendor == "unknown" && target_os == "linux" && target_env == "musl" && target_endian == "big" {
+fn is_mips_unknown_linux_musl(
+    target_arch: &str,
+    target_vendor: &str,
+    target_os: &str,
+    target_env: &str,
+    target_endian: &str,
+) -> bool {
+    if target_arch == "mips"
+        && target_vendor == "unknown"
+        && target_os == "linux"
+        && target_env == "musl"
+        && target_endian == "big"
+    {
         return true;
     }
     false
 }
 
-fn is_mips64_unknown_linux_muslabi64(target_arch: &str, target_vendor: &str, target_os: &str, target_env: &str, target_endian: &str) -> bool {
-    if target_arch == "mips64" && target_vendor == "unknown" && target_os == "linux" && target_env == "musl" && target_endian == "big" {
+fn is_mips64_unknown_linux_muslabi64(
+    target_arch: &str,
+    target_vendor: &str,
+    target_os: &str,
+    target_env: &str,
+    target_endian: &str,
+) -> bool {
+    if target_arch == "mips64"
+        && target_vendor == "unknown"
+        && target_os == "linux"
+        && target_env == "musl"
+        && target_endian == "big"
+    {
         return true;
     }
     false
 }
 
-fn is_mips64el_unknown_linux_muslabi64(target_arch: &str, target_vendor: &str, target_os: &str, target_env: &str, target_endian: &str) -> bool {
-    if target_arch == "mips64" && target_vendor == "unknown" && target_os == "linux" && target_env == "musl" && target_endian == "little" {
+fn is_mips64el_unknown_linux_muslabi64(
+    target_arch: &str,
+    target_vendor: &str,
+    target_os: &str,
+    target_env: &str,
+    target_endian: &str,
+) -> bool {
+    if target_arch == "mips64"
+        && target_vendor == "unknown"
+        && target_os == "linux"
+        && target_env == "musl"
+        && target_endian == "little"
+    {
         return true;
     }
     false
